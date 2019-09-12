fc86639b3fa8d94c75efb2683ca4b1120c36525e
Vlad Leschenko <misire351@gmail.com>
2019-09-12 20:32:16 +0300

    Welcome alert added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AlertService } from './core/services/alert.service';
+import { timer } from 'rxjs';
+import { SECOND } from './shared/constants/common';
 
 @Component({
     selector   : 'app-root',
@@ -8,4 +11,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'content-saver';
+    
+    constructor(private alert: AlertService) {
+        this.welcome();
+    }
+    
+    welcome(): void {
+        timer(SECOND).subscribe(() => this.alert.showDefaultMessage('Welcome to system, glad to see you!'));
+    }
 }
